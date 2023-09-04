bbde0faa978562a0d3e6c8ec4f3c2d94c4210196
dannnnvs <daniloborges@unipam.edu.br>
2023-09-04 11:50:42 -0300

    Menu, e grid da agenda

--- cvgPrograma/MainWindow.xaml.cs
+++ cvgPrograma/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cvgPrograma.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,25 @@ namespace cvgPrograma
         public MainWindow()
         {
             InitializeComponent();
+
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+
+            List<Service> services = new List<Service>();
+            services.Add(new Service() { Id = 4, Cliente = "Maria", ContatoCliente = 38212222, Desc = "Troca de tela", Pagamento = "Pix", Preco = 100.00f });
+            services.Add(new Service() { Id = 4, Cliente = "João", ContatoCliente = 38212223, Desc = "Troca de cabo", Pagamento = "Cartão", Preco = 50.00f });
+            services.Add(new Service() { Id = 4, Cliente = "José", ContatoCliente = 38212224, Desc = "Limpeza", Pagamento = "Dinheiro", Preco = 29.99f });
+
+            dgService.ItemsSource = services;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ cvgPrograma/Models/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvgPrograma.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        public string? Desc { get; set; }
+
+        public float Preco { get; set; }
+
+        public string? Pagamento { get; set; }
+
+        public string? Cliente { get; set; }
+
+        public int? ContatoCliente { get; set; }
+    }
+}
