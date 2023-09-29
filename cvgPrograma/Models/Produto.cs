4972b70a717198e62080b5c5afb9de57ce4ec9dd
dannnnvs <daniloborges@unipam.edu.br>
2023-09-29 17:41:18 -0300

    teste inicial de query para estoque

---
+++ cvgPrograma/Models/Produto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace cvgPrograma.Models
+{
+    public class Produto
+    {
+        private string _connectionString = "Server=localhost;Database=cvgtestedois;Uid=root;Pwd=;";
+        MySqlDataAdapter da;
+
+        public int ProdutoId { get; }
+        public string? NomeProduto { get; set; }
+
+        public int EstoqueId { get; }
+        public int QuantidadeEstoque { get; set; }
+
+        public void InserirProduto() 
+        {
+            MySqlConnection conexao = new MySqlConnection(_connectionString);
+
+            try
+            {
+                conexao.Open();
+
+                // Query para cadastrar na tabela produto
+                // @Nome e @Preco são chaves para parâmetros 
+                string inserirProdutoSql = "INSERT INTO produto (NomeProd, PrecoProd) VALUES (@Nome, @Preco);";
+                using (MySqlCommand comandoInserirProduto = new MySqlCommand(inserirProdutoSql, conexao))
+                {
+                    // "@Chave", valor de um campo input
+                    comandoInserirProduto.Parameters.AddWithValue("@Nome", 1);
+                    comandoInserirProduto.Parameters.AddWithValue("@Preco", 1); // NOMEAR AS TEXTBOX E TROCAR
+                    comandoInserirProduto.ExecuteNonQuery();
+                }
+
+                // Pega o maior id de produto (ultimo adicionado) para fazer o match no estoque
+                string consultarMaiorIdSql = "SELECT MAX(ProdId) FROM produto;";
+                using (MySqlCommand comandoConsultarMaiorId = new MySqlCommand(consultarMaiorIdSql, conexao))
+                {
+                    object resultado = comandoConsultarMaiorId.ExecuteScalar();
+
+                    if (resultado != null && resultado != DBNull.Value)
+                    {
+                        int maiorId = Convert.ToInt32(resultado);
+
+                        // Com o ID obtido, insere no estoque
+                        string inserirEstoqueSql = "INSERT INTO estoque (QuantidadeProduto, ProdId) VALUES (@Quantidade, @IdProduto);";
+                        using (MySqlCommand comandoInserirEstoque = new MySqlCommand(inserirEstoqueSql, conexao))
+                        {
+                            comandoInserirEstoque.Parameters.AddWithValue("@Quantidade", 1); // TROCAR!
+                            comandoInserirEstoque.Parameters.AddWithValue("@IdProduto", maiorId);
+                            comandoInserirEstoque.ExecuteNonQuery();
+                        }
+
+                        MessageBox.Show("Inserção concluída com sucesso.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nenhum registro encontrado na tabela 'produto'.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public DataTable ConsultarProduto()
+        {
+            MySqlConnection conexao = new MySqlConnection(_connectionString);
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            DataTable dataTable = new DataTable();
+            try 
+            {
+                conexao.Open();
+
+                string consultarProduto = "SELECT * FROM PRODUTO as p INNER JOIN estoque as e on p.ProdId = e.ProdId";
+                using (MySqlCommand comandoConsultarProduto = new MySqlCommand(consultarProduto, conexao))
+                {
+                    da.SelectCommand = comandoConsultarProduto;
+                    da.Fill(dataTable);
+                }
+                return dataTable;
+            } 
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+    }
+}
---
+++ cvgPrograma/ViewModels/EstoqueViewModel.cs
@@ -0,0 +1,23 @@
+﻿using cvgPrograma.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvgPrograma.ViewModels
+{
+    public class EstoqueViewModel
+    {
+        private Produto _produto;
+
+        public EstoqueViewModel(Produto produto)
+        {
+            _produto = produto;
+            DataTable dataTable = produto.ConsultarProduto();
+            //insere o source do datagrid
+        }
+
+    }
+}
