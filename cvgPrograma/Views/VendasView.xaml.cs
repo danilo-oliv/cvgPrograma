4d3c385ba412e1dcaeb19bbd948d01541215fc61
laysgabrielle <130411960+laysgabrielle@users.noreply.github.com>
2023-09-30 21:59:09 -0300

    Lista de vendas

--- cvgPrograma/Views/VendasView.xaml.cs
+++ cvgPrograma/Views/VendasView.xaml.cs
@@ -23,6 +23,29 @@ namespace cvgPrograma.Views
         public VendasView()
         {
             InitializeComponent();
+
+
+            List<Venda> listaVendas = new List<Venda>
+            {
+  
+                new Venda { ID="102030", Produto  ="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" },
+                new Venda { ID="102030", Produto="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" },
+                new Venda { ID="102030", Produto="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" }
+                // Adicione mais vendas conforme necessário
+            };
+            dataGridVendas.ItemsSource = listaVendas;
         }
+    }
+    public class Venda
+    {
+        public string ID { get; set; }
+        public string Produto { get; set; }
+        public string Preço { get; set; }
+        public string Pagamento { get; set; }
+        public string Cliente { get; set; }
+        public int Quantidade { get; set; }
+        public string Data { get; set; }
+        
+        
     }
 }
