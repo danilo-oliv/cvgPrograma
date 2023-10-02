5e675bcbe5ff7b0e8181ec10f096deb174ee39e2
dannnnvs <daniloborges@unipam.edu.br>
2023-10-02 15:29:19 -0300

    select das vendas ok, venda model

---
+++ cvgPrograma/ViewModels/VendasViewModel.cs
@@ -0,0 +1,40 @@
+﻿using cvgPrograma.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvgPrograma.ViewModels
+{
+    public class VendasViewModel : INotifyPropertyChanged
+    {
+
+        private DataTable _dataTableVenda;
+
+        public DataTable dataTableVenda
+        {
+            get { return _dataTableVenda; }
+            set
+            {
+                _dataTableVenda = value;
+                OnPropertyChanged(nameof(dataTableVenda));
+            }
+        }
+
+        public VendasViewModel()
+        {
+            Venda venda = new Venda();
+            dataTableVenda = venda.ConsultarVenda();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- cvgPrograma/Views/VendasView.xaml.cs
+++ cvgPrograma/Views/VendasView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cvgPrograma.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,29 +24,8 @@ namespace cvgPrograma.Views
         public VendasView()
         {
             InitializeComponent();
-
-
-            List<Venda> listaVendas = new List<Venda>
-            {
-  
-                new Venda { ID="102030", Produto  ="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" },
-                new Venda { ID="102030", Produto="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" },
-                new Venda { ID="102030", Produto="Mouse", Preço="50,00", Pagamento="Cartão de credito", Cliente="Lays", Quantidade = 5,  Data = "01/09/2023" }
-                // Adicione mais vendas conforme necessário
-            };
-            dataGridVendas.ItemsSource = listaVendas;
+            DataContext = new VendasViewModel();
         }
     }
-    public class Venda
-    {
-        public string ID { get; set; }
-        public string Produto { get; set; }
-        public string Preço { get; set; }
-        public string Pagamento { get; set; }
-        public string Cliente { get; set; }
-        public int Quantidade { get; set; }
-        public string Data { get; set; }
-        
-        
-    }
+
 }
